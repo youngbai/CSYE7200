e62f1f5f1e866823c37529e6c5bd303546e4fc28
baiyang <baiyang@uebilling.com>
2021-09-27 00:44:27 -0400

    assignment 2

---
+++ functional-programming/src/main/scala/edu/neu/coe/csye7200/Purity.java
@@ -0,0 +1,65 @@
+package edu.neu.coe.csye7200;
+
+import java.lang.reflect.Field;
+
+public class Purity {
+    class MyInteger {
+        int i;
+
+        public MyInteger(int x) {
+            i = x;
+        }
+
+        public int getInt() {
+            return i;
+        }
+
+        public void setInt(int x) {
+            i = x;
+        }
+    }
+
+    // is NOT pure, because it changed outside world, `MyInteger x`
+    public int getNext(MyInteger x) {
+        int x1 = x.getInt() + 1;
+        x.setInt(x1);
+        return x1;
+    }
+
+    // is not pure
+    public int getNext(Integer x) throws Exception {
+        int result = x.intValue() + 1;
+        Field f = x.getClass().getDeclaredField("value");
+        f.set(x, result);   // can not access `java.lang.Integer` private final member
+        return result;
+    }
+
+    // is pure
+    public int getNext(int x) {
+        x = x + 1;
+        return x;
+    }
+
+    public static void main(String[] args) {
+        Purity n = new Purity();
+        int i1 = 0;
+        testPurity(n.getNext(i1) == n.getNext(i1), "getNext(int)");
+        MyInteger i2 = n.new MyInteger(0);
+        testPurity(n.getNext(i2) == n.getNext(i2), "getNext(MyInteger)");
+        Integer i3 = 0;
+        try {
+            testPurity(n.getNext(i3) == n.getNext(i3), "getNext(Integer)");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void testPurity(boolean b, String message) {
+
+        if (b) System.out.println(message+" is idempotent");
+        else System.out.println(message+" is not idempotent");
+
+        if (b) System.out.println(message + " is pure");
+        else System.out.println(message + " is not pure");
+    }
+}

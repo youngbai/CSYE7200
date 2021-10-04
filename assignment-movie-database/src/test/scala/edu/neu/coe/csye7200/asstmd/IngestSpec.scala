132b46fa4c6810333d5eb780d0b720dfc19f161f
baiyang <baiyang@uebilling.com>
2021-10-03 23:39:52 -0400

    assignment 3

--- assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
+++ assignment-movie-database/src/test/scala/edu/neu/coe/csye7200/asstmd/IngestSpec.scala
@@ -22,7 +22,7 @@ class IngestSpec extends AnyFlatSpec with Matchers {
     val xys = ingester(source).toSeq
     // check that xys has exactly one element, consisting of Success(42) -- 10 points
     // TO BE IMPLEMENTED
-    xys.size shouldBe 1
+    xys should have size 1
     xys shouldBe Seq(Success(42))
   }
 
